48e20c08f68577de51f93856ee2996ac3726ea66
Tucker Noniewicz <tnoniewicz@comcast.net>
2026-04-10 15:34:26 -0400

    switched to classic token, hopefully this will work

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,4 +1,4 @@
 export const OWNER = "Cinntoastmin";
 export const REPO = "gamified-feedback-research";
-export const TOKEN = "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";
+export const TOKEN = "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";
 export const EXP_GRP_SIZE = 8;

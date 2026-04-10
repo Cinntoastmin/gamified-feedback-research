b1da43845192175a40cc4f3e8c5e53a498ee7cfe
Tucker Noniewicz <tnoniewicz@comcast.net>
2026-04-10 14:08:29 -0400

    Workge :D

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,5 +1,3 @@
-import { answerHandler } from "./survey";
-
 const OWNER = "Cinntoastmin";
 const REPO = "gamified-feedback-research";
 const TOKEN = "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";

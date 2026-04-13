481c8527f0f32351097666c5d0fb91e649906caf
Tucker Noniewicz <tnoniewicz@comcast.net>
2026-04-13 13:45:08 -0400

    in his name we prayge

--- assets/js/experiment.js
+++ assets/js/experiment.js
@@ -80,7 +80,7 @@ function serveFeedBack({
                 <div id="feedback-left" style="width: 20%; height: 20%; padding-right: 5%;">
                     <img src="/gamified-feedback-research/assets/png/check_mark_negative.png" alt="Correct Icon">
                 </div>
-                <span style="font-weight: bold; flex: 1;">Correct</span>             
+                <span style="font-weight: bold; align-items: end; font-size: xx-large;">Correct</span>             
             </div>
         </div>
         <button id="tutorial-continue" style="width: 100%;height: 35px">Next Page</button>
@@ -142,4 +142,3 @@ function serveFeedBack({
         callback();
     })
 }
-

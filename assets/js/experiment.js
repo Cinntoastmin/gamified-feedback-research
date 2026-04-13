5c9dcfca072e16c0870edae825ecee0edee71715
Tucker Noniewicz <tnoniewicz@comcast.net>
2026-04-13 13:24:27 -0400

    more stying

--- assets/js/experiment.js
+++ assets/js/experiment.js
@@ -75,8 +75,8 @@ function serveFeedBack({
     if (feedbackType === "CONTROL_POSITIVE"){
         expContainer.innerHTML = 
         `
-        <div style="background-color: #82fa94;border-color: #333333;border-radius: 10%;border-width: 5px;>
-            <div style="position: relative;display: flex;align-items: center;">
+        <div style="background-color: #82fa94;border-color: #000000;border-radius: 10%;border-width: 10px;>
+            <div style="position: relative;display: flex; flex-direction: row;align-items: center;">
                 <div id="feedback-left" style="width: 20%; height: 20%; padding-right: 5%;">
                     <img src="/gamified-feedback-research/assets/png/check_mark_negative.png" alt="Correct Icon">
                 </div>
